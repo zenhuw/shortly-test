bb33e32c80f3cf27eaa460f94d9d2b1c7016151e
zenhuw <zenhuwmank@gmail.com>
2023-09-11 17:24:22 +0700

    add eslint and prettier

---
+++ .eslintrc.js
@@ -0,0 +1,55 @@
+const prettierConfig = require('./.prettierrc.js');
+
+module.exports = {
+  root: true,
+  ignorePatterns: [
+    "projects/**/*"
+  ],
+  overrides: [
+    {
+      files: [
+        "*.ts"
+      ],
+      extends: [
+        "eslint:recommended",
+        "plugin:@typescript-eslint/recommended",
+        "plugin:@angular-eslint/recommended",
+        "plugin:@angular-eslint/template/process-inline-templates",
+        "plugin:prettier/recommended"
+      ],
+      plugins: ["prettier"],
+      rules: {
+        "prettier/prettier": ["warn", prettierConfig],
+        "@angular-eslint/directive-selector": [
+          "error",
+          {
+            "type": "attribute",
+            "prefix": "app",
+            "style": "camelCase"
+          }
+        ],
+        "@angular-eslint/component-selector": [
+          "error",
+          {
+            "type": "element",
+            "prefix": "app",
+            "style": "kebab-case"
+          }
+        ]
+      }
+    },
+    {
+      files: ['*.html'],
+      extends: ['plugin:@angular-eslint/template/recommended'],
+      rules: {}
+    },
+    {
+      files: ['*.html'],
+      excludedFiles: ['*inline-template-*.component.html'],
+      extends: ['plugin:prettier/recommended'],
+      rules: {
+        'prettier/prettier': ['warn', { parser: 'angular' ,...prettierConfig }]
+      }
+    }
+  ]
+};
