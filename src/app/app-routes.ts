38ca9668533341dbaf6d6725941c2f03743f82a5
zenhuw <zenhuwmank@gmail.com>
2023-09-13 15:53:05 +0700

    change route to default export

--- src/app/app-routes.ts
+++ src/app/app-routes.ts
@@ -1,10 +1,11 @@
 import { Route } from '@angular/router';
 import { AppComponent } from './app.component';
+import { LandingPageComponent } from './pages/landing-page/landing-page.component';
 
-export const routes: Route[] = [
+export default [
   {
     path: '',
     component: AppComponent,
-    children: []
+    children: [{ path: '', component: LandingPageComponent }]
   }
-];
+] as Route[];
--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { importProvidersFrom } from '@angular/core';
 import { AppComponent } from './app/app.component';
 import { BrowserModule, bootstrapApplication } from '@angular/platform-browser';
 import { provideRouter } from '@angular/router';
-import { routes } from './app/app-routes';
+import routes from './app/app-routes';
 
 bootstrapApplication(AppComponent, {
   providers: [importProvidersFrom(BrowserModule), provideRouter(routes)]
