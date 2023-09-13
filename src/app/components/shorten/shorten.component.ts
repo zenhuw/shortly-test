9eca892889f0981a22b1c1893f96bb7698a4e1d3
zenhuw <zenhuwmank@gmail.com>
2023-09-13 15:55:22 +0700

    add shorten component

---
+++ src/app/components/shorten/shorten.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-shorten',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './shorten.component.html',
+  styleUrls: ['./shorten.component.scss']
+})
+export class ShortenComponent {
+
+}
