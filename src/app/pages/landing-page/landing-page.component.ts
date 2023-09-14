8401ec565a28a7f855fef0a5f43620aa483f7fb6
zenhuw <zenhuwmank@gmail.com>
2023-09-14 10:28:33 +0700

    mobile view

--- src/app/pages/landing-page/landing-page.component.ts
+++ src/app/pages/landing-page/landing-page.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavbarComponent } from 'src/app/components/navbar/navbar.component';
 import { ShortenComponent } from 'src/app/components/shorten/shorten.component';
+import { FooterComponent } from 'src/app/components/footer/footer.component';
 
 @Component({
   standalone: true,
-  imports: [CommonModule, NavbarComponent, ShortenComponent],
+  imports: [CommonModule, NavbarComponent, ShortenComponent, FooterComponent],
   templateUrl: './landing-page.component.html',
   styleUrls: ['./landing-page.component.scss']
 })
