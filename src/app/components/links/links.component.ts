a981ecf012df2d45cddd3886c219c705ac3618ef
zenhuw <zenhuwmank@gmail.com>
2023-09-14 13:21:37 +0700

    add links component

---
+++ src/app/components/links/links.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-links',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './links.component.html',
+  styleUrls: ['./links.component.scss']
+})
+export class LinksComponent {
+
+}
