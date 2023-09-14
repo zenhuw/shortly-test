be6b4eb2a959e81a1e8d5b33be7a18b357ed668a
zenhuw <zenhuwmank@gmail.com>
2023-09-14 20:34:39 +0700

    show data from local storage

--- src/app/components/links/links.component.ts
+++ src/app/components/links/links.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { LocalStorageService } from 'src/app/services/local-storage.service';
+import { SubSink } from 'subsink/dist/subsink';
 
 @Component({
   selector: 'app-links',
@@ -8,6 +10,39 @@ import { CommonModule } from '@angular/common';
   templateUrl: './links.component.html',
   styleUrls: ['./links.component.scss']
 })
-export class LinksComponent {
+export class LinksComponent implements OnInit, OnDestroy {
+  subs = new SubSink();
 
+  urls = null;
+
+  copiedIndex: number | null = null;
+
+  constructor(private localStorageService: LocalStorageService) {}
+
+  ngOnDestroy(): void {
+    this.subs.unsubscribe();
+  }
+
+  ngOnInit(): void {
+    const json = this.localStorageService.getItem('urls');
+    let data = [];
+    if (json) {
+      data = JSON.parse(json);
+    }
+    this.urls = data.reverse();
+
+    this.subs.sink = this.localStorageService.watchStorage().subscribe(res => {
+      const json = this.localStorageService.getItem('urls');
+      let data = [];
+      if (json) {
+        data = JSON.parse(json);
+      }
+      this.urls = data.reverse();
+    });
+  }
+
+  copyLink(index: number, links: string) {
+    this.copiedIndex = index;
+    navigator.clipboard.writeText(links);
+  }
 }
