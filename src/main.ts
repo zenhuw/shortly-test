0c52e83fbf8a0fb69ec63f16632fefc70b7a1ecb
zenhuw <zenhuwmank@gmail.com>
2023-09-14 20:31:11 +0700

    add httpclientmodule

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,8 @@ import { AppComponent } from './app/app.component';
 import { BrowserModule, bootstrapApplication } from '@angular/platform-browser';
 import { provideRouter } from '@angular/router';
 import routes from './app/app-routes';
+import { HttpClientModule } from '@angular/common/http';
 
 bootstrapApplication(AppComponent, {
-  providers: [importProvidersFrom(BrowserModule), provideRouter(routes)]
+  providers: [importProvidersFrom(BrowserModule, HttpClientModule), provideRouter(routes)]
 }).catch(err => console.error(err));
