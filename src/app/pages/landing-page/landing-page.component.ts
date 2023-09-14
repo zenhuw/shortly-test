28fec2fce8bbd130c2f590242e7a877fefd4de76
zenhuw <zenhuwmank@gmail.com>
2023-09-14 20:33:16 +0700

    import links component to landing page

--- src/app/pages/landing-page/landing-page.component.ts
+++ src/app/pages/landing-page/landing-page.component.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common';
 import { NavbarComponent } from 'src/app/components/navbar/navbar.component';
 import { ShortenComponent } from 'src/app/components/shorten/shorten.component';
 import { FooterComponent } from 'src/app/components/footer/footer.component';
+import { LinksComponent } from 'src/app/components/links/links.component';
 
 @Component({
   standalone: true,
-  imports: [CommonModule, NavbarComponent, ShortenComponent, FooterComponent],
+  imports: [CommonModule, NavbarComponent, ShortenComponent, FooterComponent, LinksComponent],
   templateUrl: './landing-page.component.html',
   styleUrls: ['./landing-page.component.scss']
 })
